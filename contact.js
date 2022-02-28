64ca73b1431ad0306416e2824fbe903572fd8899
Joseph Kimani <joseph.kimani@student.moringaschool.com>
2022-02-28 10:25:51 +0300

    style:added a form on the contacts page

---
+++ contact.js
@@ -0,0 +1,14 @@
+var loginForm = document.querySelector(".login-form");
+var signUpForm = document.querySelector(".signup-form");
+var signUpE = document.querySelector("#signup");
+var loginE = document.querySelector("#login");
+
+signUpE.addEventListener("click", () => {
+    signUpForm.classList.remove("hidden-form");
+    loginForm.classList.add("hidden-form");
+});
+
+loginE.addEventListener("click", () => {
+    loginForm.classList.remove("hidden-form");
+    signUpForm.classList.add("hidden-form");
+})
